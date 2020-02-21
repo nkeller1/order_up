75863a2e1ef73fe5602c9e9efe679a464f8cb519
Nathan Keller <keller.nathan@gmail.com>
2020-02-21 10:09:04 -0700

    User stroy 2 complete. tested in both model and view.

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -23,5 +23,24 @@ RSpec.describe Dish, type: :feature do
       expect(page).to have_content(ingredient_2.name)
       expect(page).to have_content(ingredient_3.name)
     end
+
+    it "shows me the total calories of the dish" do
+
+      chef_1 = Chef.create(name: "Billy")
+
+      dish_1 = chef_1.dishes.create!(
+        name: "Fun Food",
+        description: "Really Nice",
+      )
+      ingredient_1 = Ingredient.create(name: "Frosting", calories: 600)
+      ingredient_2 = Ingredient.create(name: "Cake", calories: 50)
+      ingredient_3 = Ingredient.create(name: "sprinkles", calories: 50)
+
+      dish_1.ingredients << [ingredient_1, ingredient_2, ingredient_3]
+
+      visit "/dishes/#{dish_1.id}"
+
+      expect(page).to have_content("700 total calories")
+    end
   end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  it "calculates total calories" do
+    chef_1 = Chef.create(name: "Billy")
+
+    dish_1 = chef_1.dishes.create!(
+      name: "Fun Food",
+      description: "Really Nice",
+    )
+    ingredient_1 = Ingredient.create(name: "Frosting", calories: 600)
+    ingredient_2 = Ingredient.create(name: "Cake", calories: 50)
+    ingredient_3 = Ingredient.create(name: "sprinkles", calories: 50)
+
+    dish_1.ingredients << [ingredient_1, ingredient_2, ingredient_3]
+
+    expect(dish_1.total_calories).to eq(700)
+  end
 end
