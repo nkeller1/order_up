a242e3531c63bff62f7d282ae6574d2136aefc4a
Nathan Keller <keller.nathan@gmail.com>
2020-02-21 10:16:50 -0700

    Chef show page created. Creating route for chef/:id/ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   get "/dishes/:id", to: 'dishes#show'
 
+  get "/chefs/:id", to: 'chefs#show'
+
 end
---
+++ spec/features/chef/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Chef, type: :feature do
+  describe "chef show page" do
+    it "can see the chefs information" do
+
+      chef_1 = Chef.create(name: "Billy")
+
+      dish_1 = chef_1.dishes.create!(
+        name: "Fun Food",
+        description: "Really Nice",
+      )
+      ingredient_1 = Ingredient.create(name: "Frosting", calories: 600)
+      ingredient_2 = Ingredient.create(name: "Cake", calories: 40)
+      ingredient_3 = Ingredient.create(name: "sprinkles", calories: 50)
+
+      dish_1.ingredients << [ingredient_1, ingredient_2, ingredient_3]
+
+      visit "chefs/#{chef_1.id}"
+
+      expect(page).to have_content(chef_1.name)
+      click_link("All Ingredients")
+      expect(current_path).to eq("/chefs/#{chef_1.id}/ingredients")
+    end
+  end
+end
