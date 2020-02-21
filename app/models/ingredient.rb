b700ff8b00f56768d8af78b37228de722a40dfdc
Nathan Keller <keller.nathan@gmail.com>
2020-02-21 10:04:17 -0700

    US 1 complete.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,5 @@
 class Ingredient <ApplicationRecord
   validates_presence_of :name, :calories
-  belongs_to :chef
   has_many :dish_ingredients
   has_many :dishes, through: :dish_ingredients
 end
--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Dish, type: :feature do
         name: "Fun Food",
         description: "Really Nice",
       )
-      ingredient_1 = dish_1.ingredients.create(name: "Frosting", calories: 600)
-      ingredient_2 = dish_1.ingredients.create(name: "Cake", calories: 40)
-      ingredient_3 = dish_1.ingredients.create(name: "sprinkles", calories: 50)
+      ingredient_1 = Ingredient.create(name: "Frosting", calories: 600)
+      ingredient_2 = Ingredient.create(name: "Cake", calories: 40)
+      ingredient_3 = Ingredient.create(name: "sprinkles", calories: 50)
+
+      dish_1.ingredients << [ingredient_1, ingredient_2, ingredient_3]
 
       visit "/dishes/#{dish_1.id}"
 
       expect(page).to have_content(chef_1.name)
-      expect(page).to have_content(dish_1.name)
-      expect(page).to have_content(dish_1.description)
       expect(page).to have_content(ingredient_1.name)
       expect(page).to have_content(ingredient_2.name)
       expect(page).to have_content(ingredient_3.name)
